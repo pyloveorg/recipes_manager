d82335f0e2ac1cfcbba59ce639e4a99016dca84b
Anna Anks Nowak <anna.nowak@netguru.pl>
2018-02-11 19:18:17 +0100

    add registration

--- views.py
+++ views.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from flask import request, redirect, flash, url_for, render_template
 from main import app
 from main import db
 from main import bcrypt
 from main import lm
-
-from flask import render_template
+from models import User
 
 
 @app.route('/', methods=['GET', 'POST'])
 def info():
     return render_template('info.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    if not request.form['email'] or not request.form['password'] :
+        flash('Please enter all the fields', 'error')
+    else:
+        email = request.form['email']
+        password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('User successfully registered')
+        return redirect(url_for('login'))
+
+
