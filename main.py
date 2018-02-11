3e1eda303172b0f522e2dac7a39cde19d056e82b
Anna Anks Nowak <anna.nowak@netguru.pl>
2018-02-11 21:15:42 +0100

    some config needed for storing the session and default login view

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy()
@@ -16,10 +17,10 @@ db.app = app
 db.init_app(app)
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = 'login'
 bcrypt = Bcrypt()
 
 app.static_path = path.join(path.abspath(__file__), 'static')
-
 
 
 if __name__ == '__main__':
