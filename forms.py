994727ac5a9aefdcee7fe047b200e2f15078d1e5
Anna Anks Nowak <anna.nowak@netguru.pl>
2018-02-19 00:22:02 +0100

    correct validators

--- forms.py
+++ forms.py
@@ -1,14 +1,14 @@
 from wtforms import Form, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
-    email = StringField('Email Address', [validators.Email, validators.input_required])
+    email = StringField('Email Address', [validators.Email(message="Email is not valid"), validators.InputRequired(message="Cannot be empty")])
     password = PasswordField('Password', [
         validators.Length(min=8, message='Password too short'),
-        validators.input_required,
-        validators.equal_to('confirm', message='Passwords must match')
+        validators.InputRequired(message="Cannot be empty"),
+        validators.EqualTo('confirm', message='Passwords must match')
     ])
-    confirm = PasswordField('Confirm password', [validators.input_required])
+    confirm = PasswordField('Confirm password')
 
 class LoginForm(Form):
-    email = StringField('Email Address', [validators.Email, validators.input_required])
-    password = PasswordField('Password', [validators.input_required])
+    email = StringField('Email Address', [validators.InputRequired(message="Cannot be empty")])
+    password = PasswordField('Password', [validators.InputRequired(message="Cannot be empty")])
