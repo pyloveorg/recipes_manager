1a9f5a776a810ff9482d103d0df579e82240ec37
Anna Anks Nowak <anna.nowak@netguru.pl>
2018-02-21 23:50:55 +0100

    update recipe form to use text areas

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, IntegerField, PasswordField, RadioField, validators
+from wtforms import Form, StringField, IntegerField, PasswordField, RadioField, validators, TextAreaField
 
 class RegistrationForm(Form):
     email = StringField('Email Address', [validators.Email(message="Email is not valid"), validators.InputRequired(message="Cannot be empty")])
@@ -16,6 +16,6 @@ class LoginForm(Form):
 class RecipeForm(Form):
     title = StringField('Title', [validators.InputRequired(message="Cannot be empty")])
     time_needed = IntegerField('Time needed (min)', [validators.InputRequired(message="Cannot be empty")])
-    ingredients = StringField('Ingredients', [validators.InputRequired(message="Cannot be empty")])
-    steps = StringField('Steps', [validators.InputRequired(message="Cannot be empty")])
-    is_public = RadioField('Status', choices=[('Public', 'Public'), ('Private', 'Private')], default='Public')
+    ingredients = TextAreaField('Ingredients', [validators.InputRequired(message="Cannot be empty")])
+    steps = TextAreaField('Steps', [validators.InputRequired(message="Cannot be empty")])
+    is_public = RadioField('Status', choices=[(True, 'Public'), ('', 'Private')], default=True)
